072b03f39781277cdde486a3a991f1d50d8148d7
Rodrigo <gamba.lavin@gmail.com>
2016-10-20 15:01:45 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var app = angular.module("dcos", []);
+app.controller("dcos", function($scope,$interval,$q,$http) {
+
+    $scope.items = []
+    $scope.user="";
+    $scope.text="";
+
+    $scope.sendItem = function(){
+        data = {
+            'user' : $scope.user,
+            'text' : $scope.text
+        }
+        console.log("sending...")
+        console.log($scope.user + $scope.text);
+        $http({
+            method : 'POST',
+            url :  'http://api.dcos.com/save',
+            data : data
+        }).then(function(result){
+            $scope.user = "";
+            $scope.text = "";
+        },function(reason){
+            console.error(reason)
+        });
+    }
+
+    // Consulta periodica
+    $scope.getNewItems = function(){
+        // fecha del último post
+        console.log($scope.items)
+        if($scope.items.length <= 0){
+            last_date = '2016-01-01';
+        }else{
+            last_date = $scope.items[0].date
+        }
+        var qry = "?q="+last_date
+        $http.get('http://api.dcos.com/fetch' + qry )
+        .success(function(result){
+            if(result.items.length > 0)
+                result.items.reverse()
+                for(var i in result.items)
+                    $scope.items.unshift(result.items[i])
+
+        })
+        .error(function(data, status){
+            console.error(status)
+        });
+    }
+
+    $interval($scope.getNewItems ,500)
+
+
+});
