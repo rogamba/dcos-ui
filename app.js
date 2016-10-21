e8fdf8dc88ff9a47f138dadf9e141e9e1cb56004
Rodrigo <gamba.lavin@gmail.com>
2016-10-21 17:09:24 -0500

    Modified endpoints

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.controller("dcos", function($scope,$interval,$q,$http) {
         console.log($scope.user + $scope.text);
         $http({
             method : 'POST',
-            url :  'http://api.dcos.com/save',
+            url :  'http://dcos-api/save',
             data : data
         }).then(function(result){
             $scope.user = "";
@@ -34,7 +34,7 @@ app.controller("dcos", function($scope,$interval,$q,$http) {
             last_date = $scope.items[0].date
         }
         var qry = "?q="+last_date
-        $http.get('http://api.dcos.com/fetch' + qry )
+        $http.get('http://dcos-api/fetch' + qry )
         .success(function(result){
             if(result.items.length > 0)
                 result.items.reverse()
